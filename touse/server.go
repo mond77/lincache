81aec9abcd0584a82283e386478d88175d9da8bf
mond77 <82888636+mond77@users.noreply.github.com>
2022-10-08 18:32:44 +0800

    expose api of Starting HTTP/RPC server quickly

---
+++ touse/server.go
@@ -0,0 +1,43 @@
+package touse
+
+import (
+	"lincache"
+	"lincache/rpcconn"
+	"log"
+	"net/http"
+)
+
+func StartCacheServer(addr string, addrs []string, gee *lincache.Group) {
+	peers := lincache.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("lincache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func StartRPCServer(addr string, addrs []string, gee *lincache.Group) {
+	peers := rpcconn.NewRPCPool(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("lincache is running at", addr)
+	rpcconn.NewRPCServer(peers.Getaddr())
+}
+
+func StartAPIServer(apiAddr string, gee *lincache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+
+}
