a8728d8d82bcd8aa430a5182d7a7b6b645ae5281
mond77 <82888636+mond77@users.noreply.github.com>
2022-10-08 17:24:17 +0800

    already assure concurrent calling for the same key

--- rpcconn/client.go
+++ rpcconn/client.go
@@ -24,21 +24,22 @@ type RPCClient struct {
 	sending sync.Mutex
 
 	dest string
+
 	//wg保护还没完成的请求，保证call都完成后再关闭连接
 	wg sync.WaitGroup
 	//连接只关闭一次
 	once sync.Once
 	conn net.Conn
 
+	//pending 实现瞬时并发调用的缓存，但singleflight做到了防止缓存击穿
 	//mu sync.Mutex
-	pending map[string]*Call
-
+	//pending map[string]*Call
 	keepalive bool
 	close     bool
 }
 
 func NewRPCClient(addr string,keepalive bool) *RPCClient {
-	cli := &RPCClient{dest: addr, pending: make(map[string]*Call),keepalive: keepalive}
+	cli := &RPCClient{dest: addr,keepalive: keepalive}
 	return cli
 }
 
@@ -53,12 +54,14 @@ func (cli *RPCClient) call(req *pt.Request, resp *pt.Response)( err error) {
 	if cli.close {
 		return errors.New("cli is closed,call failure")
 	}
+	//get from pending
 	cli.wg.Add(1)
 	call := &Call{
 		req:  req,
 		resp: resp,
 		Done: make(chan *Call),
 	}
+	//
 	err = cli.send(call)
 	if err!=nil{
 		call.err = err
@@ -120,4 +123,18 @@ func (call *Call) receive(conn net.Conn) {
 
 }
 
+// func (cli *RPCClient) addcall(call *Call){
+// 	cli.mu.Lock()
+// 	defer cli.mu.Unlock()
+// 	cli.pending[call.req.GetGroup()+"-"+call.req.GetKey()] = call
+// }
+
+// func (cli *RPCClient) getcall(group,key string)*Call{
+// 	cli.mu.Lock()
+// 	defer cli.mu.Unlock()
+// 	if call,ok := cli.pending[group+"-"+key];ok{
+// 		return call
+// 	}
+// 	return nil
+// }
 
